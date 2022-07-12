9ebaaa90272610d680284d1eb3b57d7a2e5ff2a3
pyaephyokyaw15 <pyaekyaw1571999@gmail.com>
2022-07-12 22:01:22 +0630

    Delete files

--- music/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Song, Artist, Mood, Playlist, Note
-
-# Register your models here.
-admin.site.register(Song)
-admin.site.register(Artist)
-admin.site.register(Mood)
-admin.site.register(Playlist)
-admin.site.register(Note)
