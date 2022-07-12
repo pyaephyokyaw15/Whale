a28115366a77df84f9189a19c8f71833624613ba
pyaephyokyaw15 <pyaekyaw1571999@gmail.com>
2022-07-12 23:20:50 +0630

    Mood

--- songs/models.py
+++ songs/models.py
@@ -3,7 +3,15 @@ from django.db import models
 
 
 # Create your models here.
+class Mood(models.Model):
+    name = models.CharField(max_length=100)
+    banner = models.ImageField(default='images/mood_banners/default.png', upload_to='images/mood_banners/')
+
+
 class Song(models.Model):
     title = models.CharField(max_length=150)
-    banner = models.ImageField(default='song_banners/default.png', upload_to='song_banners/')
+    banner = models.ImageField(default='images/song_banners/default.png', upload_to='images/song_banners/')
     audio_file = models.FileField(upload_to='songs/')
+    mood = models.ManyToManyField(Mood)
+
+
