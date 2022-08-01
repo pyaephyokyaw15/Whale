635e29add46087334d0c6303b8b9a362f246f89c
pyaephyokyaw15 <pyaekyaw1571999@gmail.com>
2022-08-01 18:55:45 +0630

    MEDIA URL

--- whale/urls.py
+++ whale/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('account/', include('accounts.urls')),
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
